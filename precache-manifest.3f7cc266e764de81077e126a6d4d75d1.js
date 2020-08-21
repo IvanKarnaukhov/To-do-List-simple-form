53edcd83d3b907e043bdc432eb33ee77f0998830
Ivan K <69258747+IvanKarnaukhov@users.noreply.github.com>
2020-08-20 20:19:49 -0700

    Updates

---
+++ precache-manifest.3f7cc266e764de81077e126a6d4d75d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d076578af515922992d9051ec336d27",
+    "url": "/cra15/index.html"
+  },
+  {
+    "revision": "31945f4776766a38265a",
+    "url": "/cra15/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2605a2b742a179e017f1",
+    "url": "/cra15/static/js/2.b6973fea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cra15/static/js/2.b6973fea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31945f4776766a38265a",
+    "url": "/cra15/static/js/main.d0263e7b.chunk.js"
+  },
+  {
+    "revision": "c29c34a336dedd1e2715",
+    "url": "/cra15/static/js/runtime-main.0ee0c55a.js"
+  }
+]);
